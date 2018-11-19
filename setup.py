6df8d40056a9577863e2864f4953772f9788cdba
Jonas Moss <jonas.gjertsen@gmail.com>
2018-11-19 10:15:34 +0100

    :boy: First install script

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name = 'dagauss',
+      version = '0.0.1',
+      description = 'Directed acyclic graphs for multivariate Gaussians',
+      url = 'http://github.com/JonasMoss/dagauss',
+      author = 'Jonas Moss',
+      author_email = 'jonas.gjertsen@gmail.com',
+      license = 'MIT',
+      packages = ['dagauss'],
+      zip_safe = False)
